7cba7d212d73ad8fab21ab3ca67c57ee22330f93
putridamayanti <damayartdesign@gmail.com>
2025-01-01 21:23:38 +0700

    Update env issue

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,5 +1,10 @@
 import LoginForm from "components/pages/auth/LoginForm";
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
+const apiUrl = publicRuntimeConfig.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_API_URL
 
 export default function Home() {
+  console.log("Api Url: ", apiUrl)
   return <LoginForm/>;
 }
